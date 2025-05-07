11866ad3385831d23341eec0cdf558671c6166b1
Ekaterina <ekaterinaduzh@gmail.com>
2025-05-07 17:09:04 +0300

    правка yarn

--- .yarn/releases/yarn-1.22.19.cjs
+++
@@ -1,3 +0,0 @@
-$ git clone https://github.com/EkaterinaDuzh/dnd.git
-Username: EkaterinaDuzh
-Password: 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
