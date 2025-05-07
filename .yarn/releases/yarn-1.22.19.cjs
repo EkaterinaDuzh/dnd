222bcc365bade6b4ce2b7b3324af39415d85cde2
Ekaterina <ekaterinaduzh@gmail.com>
2025-05-07 16:33:36 +0300

    Добавление токена

---
+++ .yarn/releases/yarn-1.22.19.cjs
@@ -0,0 +1,3 @@
+$ git clone https://github.com/EkaterinaDuzh/dnd.git
+Username: EkaterinaDuzh
+Password: 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
